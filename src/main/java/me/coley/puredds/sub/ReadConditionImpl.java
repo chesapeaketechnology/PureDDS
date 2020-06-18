956edbc3f8e72ca42f43e33039f4654134667f14
Col-E <mcoley2@gmu.edu>
2020-06-18 13:21:23 -0400

    Implement ReadCondition's trigger value

--- src/main/java/me/coley/puredds/sub/ReadConditionImpl.java
+++ src/main/java/me/coley/puredds/sub/ReadConditionImpl.java
@@ -4,6 +4,7 @@ import org.omg.dds.core.ServiceEnvironment;
 import org.omg.dds.sub.DataReader;
 import org.omg.dds.sub.InstanceState;
 import org.omg.dds.sub.ReadCondition;
+import org.omg.dds.sub.Sample;
 import org.omg.dds.sub.SampleState;
 import org.omg.dds.sub.Subscriber;
 import org.omg.dds.sub.ViewState;
@@ -44,7 +45,20 @@ public class ReadConditionImpl<T> implements ReadCondition<T> {
 
 	@Override
 	public boolean getTriggerValue() {
-		// TODO: How to determine trigger value?
+		// From the javadoc:
+		// "The triggerValue of the ReadCondition is tied to the presence of at least
+		//  a sample with SampleState, ViewState, and InstanceState matching those of the ReadCondition"
+		Set<InstanceState> instanceStates = states.getInstanceStates();
+		Set<ViewState> viewStates = states.getViewStates();
+		Set<SampleState> sampleStates = states.getSampleStates();
+		for (Sample.Iterator<T> it = parent.read(); it.hasNext(); ) {
+			Sample<T> sample = it.next();
+			if (instanceStates.contains(sample.getInstanceState()) &&
+					viewStates.contains(sample.getViewState()) &&
+					sampleStates.contains(sample.getSampleState())) {
+				return true;
+			}
+		}
 		return false;
 	}
 
